82a5c1f4e08c0659f99ba415225f1cb62eb1147d
bizhiwei <bizhiweii@163.com>
2016-05-26 00:45:20 +0800

    Initial commit

---
+++ app/src/main/java/com/gentlemanb/shinelon/openglvrapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.gentlemanb.shinelon.openglvrapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    private OpenGLView mOpenGLView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 去标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mOpenGLView = new OpenGLView(this);
+        setContentView(mOpenGLView);
+    }
+}
